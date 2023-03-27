8891323be271616d16a20f74d5000a1e3e1592ce
Kang Ah <ahkang02@outlook.com>
2023-03-27 21:56:39 +0800

    Database Added

---
+++ MedicalStore/Admin/Dashboard.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="MedicalStore.Admin.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    
+    <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged1"></asp:GridView>
+</asp:Content>
---
+++ MedicalStore/Admin/Dashboard.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MedicalStore.Admin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.Columns.Add(new System.Data.DataColumn("DummyColum1", typeof(String)));
+
+            dr = dt.NewRow();
+            dr[0] = "A dummy Data";  //Adds the Dummy Data in the Row
+            dt.Rows.Add(dr);
+
+
+            // Show the DataTable values in the GridView
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
