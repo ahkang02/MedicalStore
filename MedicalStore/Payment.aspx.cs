7f60b29d264f5f9f2c1a880836749b1ef32151fc
Ng Zhan Qing <87813625+Angelz878@users.noreply.github.com>
2023-05-07 20:30:10 +0800

    Update for payment simulator get session

--- MedicalStore/Payment.aspx.cs
+++ MedicalStore/Payment.aspx.cs
@@ -59,83 +59,101 @@ namespace MedicalStore
 
         protected void btnCheckout_Click(object sender, EventArgs e)
         {
-            // Assuming you have a connection string defined in your web.config file
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            // Retrieve the payment information from the session
+            decimal paymentAmount = Convert.ToDecimal(Session["TotalPaymentAmount"]);
+            string selectedPaymentMethod = paymentMethod.SelectedValue;
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            // Generate a new payment ID
+            string newPaymentID = GenerateNewPaymentID();
+
+            // Store the payment details in individual session variables
+            Session["PaymentID"] = newPaymentID;
+            Session["PaymentMethod"] = selectedPaymentMethod;
+            Session["PaymentAmount"] = paymentAmount;
+            Session["PaymentDateTime"] = DateTime.Now;
+
+
+
+            // Redirect to the appropriate payment simulator page based on the selected payment method
+            string redirectUrl = "";
+
+            switch (selectedPaymentMethod)
             {
-                try
-                {
-                    connection.Open();
-
-                    // Create a SqlCommand to insert the payment details into the database
-                    SqlCommand command = new SqlCommand("INSERT INTO Payment (PaymentID, PaymentMethod, PaymentAmount, PaymentDateTime) VALUES (@PaymentID, @PaymentMethod, @PaymentAmount, @PaymentDateTime)", connection);
-
-                    // Retrieve the payment information from the session
-                    decimal paymentAmount = Convert.ToDecimal(Session["TotalPaymentAmount"]);
-                    string selectedPaymentMethod = paymentMethod.SelectedValue;
-
-                    // Generate a new payment ID
-                    string newPaymentID = GenerateNewPaymentID(connection);
-
-                    // Set the parameter values
-                    command.Parameters.AddWithValue("@PaymentID", newPaymentID);
-                    command.Parameters.AddWithValue("@PaymentMethod", selectedPaymentMethod);
-                    command.Parameters.AddWithValue("@PaymentAmount", paymentAmount);
-                    command.Parameters.AddWithValue("@PaymentDateTime", DateTime.Now);
-
-                    // Execute the query
-                    command.ExecuteNonQuery();
-
-                    // Redirect to the appropriate payment simulator page based on the selected payment method
-                    string redirectUrl = "";
-
-                    switch (selectedPaymentMethod)
-                    {
-                        case "Credit":
-                            redirectUrl = $"PaymentSimulator.aspx?paymentMethod=credit&paymentID={newPaymentID}";
-                            break;
-                        case "FPX":
-                            redirectUrl = $"PaymentSimulator.aspx?paymentMethod=fpx&paymentID={newPaymentID}";
-                            break;
-                        case "E-Wallet":
-                            redirectUrl = $"PaymentSimulator.aspx?paymentMethod=ewallet&paymentID={newPaymentID}";
-                            break;
-                        case "Cash":
-                            redirectUrl = $"PaymentSimulator.aspx?paymentMethod=cod&paymentID={newPaymentID}";
-                            break;
-                    }
-
-                    Response.Redirect(redirectUrl);
-                }
-                catch (Exception ex)
-                {
-                    // Handle any exceptions that occur during the database connection or query execution
-                    // Display an error message to the user or perform any other necessary actions
-                }
+                case "Credit":
+                    redirectUrl = $"PaymentSimulator.aspx?paymentMethod=credit";
+                    break;
+                case "FPX":
+                    redirectUrl = $"PaymentSimulator.aspx?paymentMethod=fpx";
+                    break;
+                case "E-Wallet":
+                    redirectUrl = $"PaymentSimulator.aspx?paymentMethod=ewallet";
+                    break;
+                case "Cash":
+                    redirectUrl = $"PaymentSimulator.aspx?paymentMethod=cod";
+                    break;
             }
+
+            Response.Redirect(redirectUrl);
         }
 
 
+
         // Generate a new payment ID
-        private string GenerateNewPaymentID(SqlConnection connection)
+        private string GenerateNewPaymentID()
         {
+            // Check if a payment ID already exists in the session
+            string paymentID = Session["PaymentID"] as string;
+            if (!string.IsNullOrEmpty(paymentID))
+            {
+                return paymentID; // Return the existing payment ID
+            }
+
             // Retrieve the latest payment ID from the database
-            SqlCommand command = new SqlCommand("SELECT MAX(CAST(RIGHT(PaymentID, LEN(PaymentID) - 1) AS INT)) FROM Payment", connection);
-            string latestPaymentID = command.ExecuteScalar()?.ToString();
+            string latestPaymentID = GetLatestPaymentIDFromDatabase();
 
             // Generate the new payment ID
             int newPaymentIndex = 1;
             if (!string.IsNullOrEmpty(latestPaymentID))
             {
-                int.TryParse(latestPaymentID, out int lastIndex);
-                newPaymentIndex = lastIndex + 1;
+                int.TryParse(latestPaymentID.Substring(1), out newPaymentIndex);
+                newPaymentIndex++; // Increment the index by 1
             }
 
             string newPaymentID = "P" + newPaymentIndex.ToString("D3"); // Format the index as a 3-digit number with leading zeros
 
+
             return newPaymentID;
         }
+
+        // Retrieve the latest payment ID from the database
+        private string GetLatestPaymentIDFromDatabase()
+        {
+            // Assuming you have a connection string defined in your web.config file
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            string latestPaymentID = "";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT TOP 1 PaymentID FROM Payment ORDER BY PaymentID DESC";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    latestPaymentID = reader["PaymentID"].ToString();
+                }
+
+                reader.Close();
+                connection.Close();
+            }
+
+            return latestPaymentID;
+        }
+
+
 
 
 
