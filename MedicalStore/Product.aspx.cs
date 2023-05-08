b351560fb47577a19a68d3a6565fd72713fd2dc6
Jielujun <86587108+Jielujun@users.noreply.github.com>
2023-05-09 03:32:37 +0800

    Update Product.aspx.cs

--- MedicalStore/Product.aspx.cs
+++ MedicalStore/Product.aspx.cs
@@ -17,6 +17,69 @@ namespace MedicalStore
            
         }
 
+        protected void addNewOrderButton_Click(object sender, EventArgs e)
+        {
+
+            //SqlConnection con3;
+            //string strCon3 = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            //con3 = new SqlConnection(strCon3);
+            //con3.Open();
+            //string strGetID = "SELECT MAX(SUBSTRING(OrderID, 2, 4)) AS OrderID FROM Orders";
+            //SqlCommand command2 = new SqlCommand(strGetID, con3);
+            //string getNextMaxID = (string)command2.ExecuteScalar();
+            //int ID = Convert.ToInt32(getNextMaxID) + 1;
+            //string nextID = ID.ToString("000");
+            //con3.Close();
+
+
+
+
+            //SqlConnection con;
+            //string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            //// Insert Statement Into DB
+            //con = new SqlConnection(strCon);
+
+           
+            ////float price = float.Parse(button.CommandName, NumberStyles.Currency, CultureInfo.InvariantCulture);
+
+            //string OrderID = "";
+
+            //SqlConnection con5;
+            //string strCon5 = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            //// Insert Statement Into DB
+            //con5 = new SqlConnection(strCon5);
+
+            //using (SqlConnection connection = new SqlConnection(strCon5))
+            //{
+
+
+            //    string InsertQuery = "INSERT INTO Orders (OrderID) " +
+            //        "VALUES (@OrderID)";
+
+
+            //    using (SqlCommand command = new SqlCommand(InsertQuery, connection))
+            //    {
+            //        // set the parameter value for productId
+            //        command.Parameters.AddWithValue("@OrderID", OrderID);
+
+
+            //        // open the connection and execute the command
+            //        connection.Open();
+            //        int rowsAffected = command.ExecuteNonQuery();
+
+
+            //    }
+
+
+            //}
+
+            //con5.Close();
+
+        }
+
         protected void addToCartButton_Click(object sender, EventArgs e)
         {
 
@@ -28,10 +91,12 @@ namespace MedicalStore
             string strGetID = "SELECT MAX(SUBSTRING(OrderID, 2, 4)) AS OrderID FROM Orders";
             SqlCommand command2 = new SqlCommand(strGetID, con2);
             string getNextMaxID = (string)command2.ExecuteScalar();
-            int ID = Convert.ToInt32(getNextMaxID) + 1;
+            int ID = Convert.ToInt32(getNextMaxID) ;
             string nextID = ID.ToString("000");
             con2.Close();
 
+
+            string OrderID = "";
 
 
 
@@ -47,7 +112,6 @@ namespace MedicalStore
             float price = float.Parse(button.CommandName);
             //float price = float.Parse(button.CommandName, NumberStyles.Currency, CultureInfo.InvariantCulture);
 
-            string OrderID = "";
             int quantity = 1;
             using (SqlConnection connection = new SqlConnection(strCon))
             {
