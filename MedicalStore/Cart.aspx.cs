acec2e4b7cb2e09f56df68a577d339e9c816f33d
Jielujun <86587108+Jielujun@users.noreply.github.com>
2023-05-09 03:33:36 +0800

    Update Cart.aspx.cs

--- MedicalStore/Cart.aspx.cs
+++ MedicalStore/Cart.aspx.cs
@@ -23,7 +23,68 @@ namespace MedicalStore
         {
             
         }
+        protected void addNewOrderButton_Click(object sender, CommandEventArgs e)
+        {
 
+            SqlConnection con3;
+            string strCon3 = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            con3 = new SqlConnection(strCon3);
+            con3.Open();
+            string strGetID = "SELECT MAX(SUBSTRING(OrderID, 2, 4)) AS OrderID FROM Orders";
+            SqlCommand command2 = new SqlCommand(strGetID, con3);
+            string getNextMaxID = (string)command2.ExecuteScalar();
+            int ID = Convert.ToInt32(getNextMaxID) + 1;
+            string nextID = ID.ToString("000");
+            con3.Close();
+
+
+
+
+            SqlConnection con;
+            string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            // Insert Statement Into DB
+            con = new SqlConnection(strCon);
+
+
+            //float price = float.Parse(button.CommandName, NumberStyles.Currency, CultureInfo.InvariantCulture);
+
+            string OrderID = "";
+
+            SqlConnection con5;
+            string strCon5 = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            // Insert Statement Into DB
+            con5 = new SqlConnection(strCon5);
+
+            using (SqlConnection connection = new SqlConnection(strCon5))
+            {
+                OrderID = 'O' + nextID;
+
+                string InsertQuery = "INSERT INTO Orders (OrderID) " +
+                    "VALUES (@OrderID)";
+
+
+                using (SqlCommand command = new SqlCommand(InsertQuery, connection))
+                {
+                    // set the parameter value for productId
+                    command.Parameters.AddWithValue("@OrderID", OrderID);
+
+
+                    // open the connection and execute the command
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+
+
+                }
+
+
+            }
+
+            con5.Close();
+
+        }
 
         protected void btnCheckout_Click(object sender, EventArgs e)
         {
@@ -61,21 +122,32 @@ namespace MedicalStore
                 con.Close();
             }
 
-  
+
+            SqlConnection con6;
+            string strCon6 = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            con6 = new SqlConnection(strCon6);
+            con6.Open();
+            string strGetID = "SELECT MAX(SUBSTRING(OrderID, 2, 4)) AS OrderID FROM Orders";
+            SqlCommand command2 = new SqlCommand(strGetID, con6);
+            string getNextMaxID = (string)command2.ExecuteScalar();
+            int ID = Convert.ToInt32(getNextMaxID);
+            string nextID = ID.ToString("000");
+            con6.Close();
 
 
+            string orderID = " ";
 
-            string orderID = "O011";
             float totalAmount = float.Parse(Request.Form["hdnTotal"]);
             string status = Request.Form["hdnDelivery"];
             DateTime orderDate = DateTime.Today;
             string customerID = "C001";
-            
 
-          
-            
+            orderID = 'O' + nextID;
 
-                SqlConnection con3;
+
+
+            SqlConnection con3;
                 string strCon3 = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
 
                 // Insert Statement Into DB
@@ -85,8 +157,10 @@ namespace MedicalStore
                 {
 
 
-                    string InsertQuery = "INSERT INTO Orders (OrderID, OrderDate, TotalAmount,Status,CustomerID) " +
-                        "VALUES (@OrderID, @OrderDate, @TotalAmount,@Status,@CustomerID)";
+                    string InsertQuery = "UPDATE Orders SET OrderDate=@OrderDate, TotalAmount=@TotalAmount, Status=@Status, CustomerID=@CustomerID WHERE OrderID=@OrderID";
+             
+                //"INSERT INTO Orders (OrderID, OrderDate, TotalAmount,Status,CustomerID) " +
+                //        "VALUES (@OrderID, @OrderDate, @TotalAmount,@Status,@CustomerID)";
 
 
                     using (SqlCommand command = new SqlCommand(InsertQuery, connection))
