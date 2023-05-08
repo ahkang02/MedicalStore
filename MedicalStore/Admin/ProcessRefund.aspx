d60c66fa2d8a8ba74b4ebca06a863a5e724dcdbf
Kang Ah <ahkang02@outlook.com>
2023-05-08 17:55:46 +0800

    Process Refund Completed

--- MedicalStore/Admin/ProcessRefund.aspx
+++ MedicalStore/Admin/ProcessRefund.aspx
@@ -1,7 +1,36 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Customer.Master" AutoEventWireup="true" CodeBehind="ProcessRefund.aspx.cs" Inherits="MedicalStore.Admin.ProcessRefund" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="ProcessRefund.aspx.cs" Inherits="MedicalStore.Admin.ProcessRefund" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
-</asp:Content>
-<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="ms-3">
+        <h2>Refund Management</h2>
+    </div>
+    <!-- Repeater -->
+    <div class="container-fluid">
+        <asp:Repeater ID="repeatUser" runat="server" OnItemCommand="repeatUser_ItemCommand">
+            <HeaderTemplate>
+                <table class="table table-hover table-responsive table-bordered">
+                    <tr>
+                        <th>Refund ID</th>
+                        <th>Refund Status</th>
+                        <th>Refund Reason</th>
+                        <th>Actions</th>
+                    </tr>
+            </HeaderTemplate>
+
+            <ItemTemplate>
+                <tr>
+                    <td><%#DataBinder.Eval(Container.DataItem,"RefundID")%></td>
+                    <td><%#DataBinder.Eval(Container.DataItem,"Status")%></td>
+                    <td><%#DataBinder.Eval(Container.DataItem, "Reason") %></td>
+                    <td>
+                        <asp:Button ID="btnEdit" CssClass="btn btn-sm btn-primary ms-2" Text="Approve" runat="server" CommandName="Approve" CommandArgument='<%#Eval ("RefundID") %>' UseSubmitBehavior="false" CausesValidation="false" />
+                        <asp:Button ID="btnDelete" CssClass="btn btn-sm btn-danger ms-2" Text="Reject" runat="server" CommandName="Reject" CommandArgument='<%#Eval ("RefundID") %>' UseSubmitBehavior="false" OnClientClick="if (!confirm('Are you sure you want reject?')) return false;" CausesValidation="false"/>
+                </tr>
+            </ItemTemplate>
+        </asp:Repeater>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Refund]"></asp:SqlDataSource>
+    </div>
 </asp:Content>
--- MedicalStore/Admin/ProcessRefund.aspx.cs
+++ MedicalStore/Admin/ProcessRefund.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +11,70 @@ namespace MedicalStore.Admin
 {
     public partial class ProcessRefund : System.Web.UI.Page
     {
+        SqlConnection conn;
+        string strCon = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            repeatUser.DataSource = SqlDataSource1;
+            repeatUser.DataBind();
+        }
+
+        protected void repeatUser_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            string refundID = e.CommandArgument.ToString();
+
+            switch (e.CommandName)
+            {
+                case "Approve":
+                    conn = new SqlConnection(strCon);
+                    conn.Open();
+
+                    string strUpdate = "UPDATE Refund SET Status = @Status WHERE RefundID = @ID";
+                    SqlCommand command = new SqlCommand(strUpdate, conn);
+                    command.Parameters.AddWithValue("@Status", "Approve");
+                    command.Parameters.AddWithValue("@ID", refundID);
+                    int n = command.ExecuteNonQuery();
+
+                    if (n > 0)
+                    {
+                        ClientScript.RegisterClientScriptBlock(this.GetType(), "Success", "alert('Refund Approved.');", true);
+                        Response.Redirect("ProcessRefund.aspx");
+                    }
+                    else
+                    {
+                        ClientScript.RegisterClientScriptBlock(this.GetType(), "Failed", "alert('Caught an error');", true);
+                    }
+                    conn.Close();
+                    break;
+
+                case "Reject":
+
+                    conn = new SqlConnection(strCon);
+                    conn.Open();
+
+                    string strUpdate1 = "UPDATE Refund SET Status = @Status WHERE RefundID = @ID";
+                    SqlCommand command1 = new SqlCommand(strUpdate1, conn);
+                    command1.Parameters.AddWithValue("@Status", "Rejected");
+                    command1.Parameters.AddWithValue("@ID", refundID);
+                    int n1 = command1.ExecuteNonQuery();
+
+                    if (n1 > 0)
+                    {
+                        ClientScript.RegisterClientScriptBlock(this.GetType(), "Success", "alert('Refund Rejected.');", true);
+                        Response.Redirect("ProcessRefund.aspx");
+                    }
+                    else
+                    {
+                        ClientScript.RegisterClientScriptBlock(this.GetType(), "Failed", "alert('Caught an error');", true);
+                    }
+                    conn.Close();
+
+                    break;
+
+                default:
+                    break;
+            }
 
         }
     }
